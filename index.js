7ae2b1b13f74a00f8416f0ba627cd3606ade6290
Lucasdmottola <lucasdoege@hotmail.com>
2023-05-16 17:08:03 -0300

    funções de edição

--- index.js
+++ index.js
@@ -47,27 +47,17 @@ let catalogo_usuarios = ['sandy', 'jneto', 'chico', 'bento'];
 		function editar_dados() {
 			if (ctrl_login && ctrl_senha) { // Verifica se o login e senha foram validados
 				let index = catalogo_usuarios.indexOf(document.getElementById("usuario").value.trim());
-				if (index >= 0) { // Verifica se o usuário existe no catálogo
-					document.getElementById("usuario").readOnly = false;
-					document.getElementById("senha").readOnly = false;
-					document.getElementById("btnEditar").style.display = "none";
-					document.getElementById("btnCancelar").style.display = "";
-					console.log("Campos de login e senha habilitados para edição.");
-				} else {
-					console.log("Erro: Usuário não encontrado.");
+				if (????) { // Verifica se o usuário existe no sistema
+					
+				
+				//console.log("Erro: Usuário não encontrado.");
+			
+				//console.log("Erro: Realize a validação de login e senha antes de editar seus dados.");
+			
 				}
-			} else {
-				console.log("Erro: Realize a validação de login e senha antes de editar seus dados.");
-			}
 		}
 
 		function cancelar_edicao() {
-			document.getElementById("usuario").readOnly = true;
-			document.getElementById("senha").readOnly = true;
-			document.getElementById("btnEditar").style.display = "";
-			document.getElementById("btnCancelar").style.display = "none";
+			
 			console.log("Edição de campos cancelada.");
 		}
-
-
-		
